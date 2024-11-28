cd2efbf4f73357592752ad26a7534d01cc9a6426
ymg2003 <92130097@students.liu.edu.lb>
2024-11-28 19:12:42 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,39 @@
 
+import 'package:flutter/material.dart';
+import 'welcome_page.dart';
+
+void main() {
+  runApp(GiftGeneratorApp());
+}
+
+class GiftGeneratorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gift Generator',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.red,
+          accentColor: Colors.yellow,
+        ),
+        scaffoldBackgroundColor: Colors.yellow[50],
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.red,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: WelcomePage(),
+    );
+  }
+}
