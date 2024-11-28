47b7d7da944567865e619986e1ba4cce2e04dbf9
ymg2003 <92130097@students.liu.edu.lb>
2024-11-28 19:09:53 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
-import 'home_page.dart';
 
-void main() {
-  runApp(GiftGeneratorApp());
-}
-
-class GiftGeneratorApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Gift Generator',
-      home: HomePage(),
-    );
-  }
-}
