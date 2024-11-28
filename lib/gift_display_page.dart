d8534d831f8dda26c26279d823fc66b4bf2e0756
ymg2003 <92130097@students.liu.edu.lb>
2024-11-28 19:14:26 +0200

    Update gift_display_page.dart

--- lib/gift_display_page.dart
+++ lib/gift_display_page.dart
@@ -5,13 +5,18 @@ class GiftDisplayPage extends StatelessWidget {
   final String giftPrice;
   final String giftDescription;
 
-  GiftDisplayPage({required this.giftName, required this.giftPrice, required this.giftDescription});
+  GiftDisplayPage({
+    required this.giftName,
+    required this.giftPrice,
+    required this.giftDescription,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Suggested Gift'),
+        backgroundColor: Colors.red,
       ),
       body: Center(
         child: Column(
@@ -19,12 +24,12 @@ class GiftDisplayPage extends StatelessWidget {
           children: [
             Text(
               'Suggested Gift: $giftName',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.red[800]),
             ),
             SizedBox(height: 20),
             Text(
               'Price: $giftPrice',
-              style: TextStyle(fontSize: 18),
+              style: TextStyle(fontSize: 18, color: Colors.red[700]),
             ),
             SizedBox(height: 10),
             Padding(
@@ -32,7 +37,7 @@ class GiftDisplayPage extends StatelessWidget {
               child: Text(
                 giftDescription,
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16),
+                style: TextStyle(fontSize: 16, color: Colors.red[600]),
               ),
             ),
           ],
