f8552c9500a180054f24cc1fa43a67567b8e15d0
Maryam92210054 <92210054@students.liu.edu.lb>
2024-11-29 13:05:14 +0200

    Update gift_display_page.dart

--- lib/gift_display_page.dart
+++ lib/gift_display_page.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 
 class GiftDisplayPage extends StatelessWidget {
   final String giftName;
-  final String giftPrice;
   final String giftDescription;
 
   GiftDisplayPage({
     required this.giftName,
-    required this.giftPrice,
     required this.giftDescription,
   });
 
@@ -16,7 +14,6 @@ class GiftDisplayPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Suggested Gift'),
-        backgroundColor: Colors.red,
       ),
       body: Center(
         child: Column(
@@ -24,12 +21,7 @@ class GiftDisplayPage extends StatelessWidget {
           children: [
             Text(
               'Suggested Gift: $giftName',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.red[800]),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Price: $giftPrice',
-              style: TextStyle(fontSize: 18, color: Colors.red[700]),
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
             Padding(
@@ -37,7 +29,7 @@ class GiftDisplayPage extends StatelessWidget {
               child: Text(
                 giftDescription,
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16, color: Colors.red[600]),
+                style: TextStyle(fontSize: 25, color: Colors.grey),
               ),
             ),
           ],
